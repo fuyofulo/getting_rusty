9a626cd63c4fe5b01ba22da69a2e4eaee0599f10
fuyofulo <pheonixdiaz625@gmail.com>
2025-03-01 16:09:59 +0530

    generics and traits

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,9 +1,16 @@
 fn main() {
     display_stuff(1, 2);
     display_stuff(String::from("hello"), String::from("World!"));
+
+    println!("{}", sum(1, 2));
+    println!("{}", sum(5.0, 6.0));
 }
 
 fn display_stuff<T: std::fmt::Display> (a: T, b: T) {
     println!("{a}");
     println!("{b}");
+}
+
+fn sum<T: std::ops::Add<Output = T>>(a: T, b: T) -> T {
+    return a + b;
 }
